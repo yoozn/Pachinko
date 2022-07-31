a860fd6ed8ac3e57a20ab3f2237c694a1d091ff4
yooozn <brock.young@gedu.sd73.bc.ca>
2022-07-31 09:39:32 -0700

    Added settings from all scenes.

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -45,7 +45,10 @@ public class Settings : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleSettings();
+        }
     }
 
     public void ToggleSettings()
